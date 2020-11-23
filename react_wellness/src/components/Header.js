d17561cc12cbfe7a4faf1e6ae1e20288a4850896
claireabely <71153552+claireabely@users.noreply.github.com>
2020-11-23 15:13:17 -0500

    heroku

--- react_wellness/src/components/Header.js
+++ react_wellness/src/components/Header.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-class Header extends Component {
+const { Header, Content, Footer } = Layout;
+
+class HeaderDemo extends Component {
   render() {
     return (
-      <header>
-        <h1>Food App</h1>
+      
+      <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
        
-      </header>
+        </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 , color: 'black',}}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        Content
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
     )
   }
 }
