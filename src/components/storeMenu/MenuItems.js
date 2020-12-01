8549b0fb22e039086fb32e8f570781e498f88358
claireabely <71153552+claireabely@users.noreply.github.com>
2020-12-01 09:27:38 -0500

    commit

--- src/components/storeMenu/MenuItems.js
+++ src/components/storeMenu/MenuItems.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Food from './Food'
-import { Card, Avatar } from 'antd';
+import { Card, } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
