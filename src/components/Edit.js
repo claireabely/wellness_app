fac2f838ee4c38709580cab2a875c0027e65ee7d
claireabely <71153552+claireabely@users.noreply.github.com>
2020-11-30 22:19:38 -0500

    updates

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -3,9 +3,17 @@ class Edit extends Component {
   render() {
     return (
       <div className="Edit">
-             { this.props.orders.map((orders, index) => {
-                 
-             })}
+              <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">mode_edit</i>
+          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+          <label for="icon_prefix2">First Name</label>
+        </div>
+      </div>
+    </form>
+  </div>
    
       </div>
     )
