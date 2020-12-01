1b0db27e426f0f0442b0bdb9b95cc03f0752d147
claireabely <71153552+claireabely@users.noreply.github.com>
2020-12-01 00:37:12 -0500

    updates

--- src/components/storeMenu/Food.js
+++ src/components/storeMenu/Food.js
@@ -8,7 +8,7 @@ export default class Food extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            love: true
+            purchase: true
         }
 
     }
@@ -20,13 +20,13 @@ export default class Food extends Component {
             <thead>
                 
             <tr key={ this.props.index }
-                onClick={ ()=> { this.props.addLovedFood(this.props.food)(this.state.order); }}
+                onClick={ ()=> { this.props.addPurchasedFood(this.props.food)(this.state.order); }}
             >
                 <td onClick={() => this.props.handleDelete(this.props.index)} >{ this.props.food.name }</td>
                 <td>{ this.props.food.description }</td>
                 {/* <td>{ this.props.food.imgURL }</td> */}
                 <td>{ this.props.food.price }</td>
-                {this.state.love ? <td><PlusCircleOutlined /></td> : <td></td>}
+                {this.state.purchase ? <td><PlusCircleOutlined /></td> : <td></td>}
             </tr>
             </thead>
            
